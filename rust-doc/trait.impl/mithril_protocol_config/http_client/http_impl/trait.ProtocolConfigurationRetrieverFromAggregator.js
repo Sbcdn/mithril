02b71e7b3c4dd4a5be5ee5477b091648c1c89fc0
Alenar <Alenar@users.noreply.github.com>
2025-10-31 11:09:56 +0000

    deploy: 06256213f9eb884a895eb8d44270715b4554565b

---
+++ rust-doc/trait.impl/mithril_protocol_config/http_client/http_impl/trait.ProtocolConfigurationRetrieverFromAggregator.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["mithril_signer",[["impl ProtocolConfigurationRetrieverFromAggregator for <a class=\"struct\" href=\"mithril_signer/services/struct.AggregatorHTTPClient.html\" title=\"struct mithril_signer::services::AggregatorHTTPClient\">AggregatorHTTPClient</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[253]}
